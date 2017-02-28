538204d6df80f9810982e0f569361db94c684c53
Michail Beliatis <grapheneintelligentsystems@gmail.com>
2017-03-01 00:30:56 +0100

    delete dph.php
    remove info about connection

--- dph.php
+++
@@ -1,9 +0,0 @@
-<!-- http://localhost/wpdevmate/wpdm-websites/gis.com.dev/wp-content/themes/MB-child/dph.php -->
-<?php
-$conn = mysqli_connect("localhost", "root", "", "logintest");
-
-if(!$conn){
-  die("Connection failed: ".mysqli_connect_error());
-}
-
-?>
