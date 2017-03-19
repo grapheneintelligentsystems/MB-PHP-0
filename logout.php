c25d15140d4bd1b8f0cf80af5b645cf2ed457dca
grapheneintelligentsystems <grapheneintelligentsystems@gmail.com>
2017-03-19 12:54:22 +0100

    updated deleted files

--- logout.php
+++
@@ -1,6 +0,0 @@
-<!-- http://localhost/wpdevmate/wpdm-websites/gis.com.dev/wp-content/themes/MB-child/logout.php -->
-<?php
-session_start();
-session_destroy();
-header("Location: indexMB.php");
-?>
